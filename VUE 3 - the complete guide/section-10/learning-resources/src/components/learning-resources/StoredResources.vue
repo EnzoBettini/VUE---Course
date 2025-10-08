cfc03f81580bbf2cc49a03545006e45b61f32e64
Enzo Ayres Bettini <enzobettini@hotmail.com>
2025-10-08 17:11:53 -0300

    Finishing Lessons 08/10/2025

---
+++ VUE 3 - the complete guide/section-10/learning-resources/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul>
+        <LearningResource
+        v-for="storedResource in resources"
+        :key="storedResource.id"
+        :id="storedResource.id"
+        :title="storedResource.title"
+        :description="storedResource.description"
+        :link="storedResource.link"
+        >
+        </LearningResource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+    components: {
+        LearningResource
+    },
+    inject: ['resources'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
